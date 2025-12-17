a6e02fa8c70ced18e500dbd5520d92961e677aee
keithm1330 <keith13@live.ie>
2025-12-17 15:15:48 +0000

    first

---
+++ src/main/java/com/example/round_robin/RoundRobin21Application.java
@@ -0,0 +1,13 @@
+package com.example.round_robin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundRobin21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundRobin21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/round_robin/RoundRobin21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.round_robin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundRobin21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
